298ad270fb4172696263682c08af80dab7478d6c
mrunbanked <mrunbanked@protonmail.com>
2023-01-04 15:07:14 +0000

    Add bool

--- ethers-contract/src/multicall/mod.rs
+++ ethers-contract/src/multicall/mod.rs
@@ -749,7 +749,7 @@ impl<M: Middleware> Multicall<M> {
                             let mut res_tokens = call
                                 .function
                                 .decode_output(bytes)
-                                .unwrap_or(vec![Token::Uint(U256::zero())]);
+                                .unwrap_or(vec![Token::Bool(false), Token::Uint(U256::zero())]);
                             match res_tokens.len() {
                                 0 => Token::Tuple(vec![]),
                                 1 => res_tokens.remove(0),
